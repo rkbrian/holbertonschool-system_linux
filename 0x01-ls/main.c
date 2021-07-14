664505490a38122ef9a6900bb8eff1e27fc5eb4f
rkbrian <rkbriankong@gmail.com>
2021-07-14 23:04:43 +0000

    first test

--- 0x01-ls/main.c
+++ 0x01-ls/main.c
@@ -4,13 +4,30 @@
 #include <dirent.h>
 
 /**
- * main - function to list the content of the current directory
+ * main - function to list the content of the current directory and more
+ * @argc: argument count
+ * @argv: argument array
  * Retrun: 0
  */
 int main(int argc, char *argv[])
 {
+  struct dirent *mydir_stream;
   DIR *dir = opendir(".");
-  return 0;
 
+  (void)argc;
+  (void)argv;
+  if (dir == NULL)
+  {
+    return (1);
+  }
 
+  mydir_stream = readdir(dir);
+  while (mydir_stream != NULL)
+  {
+    printf("%s-", mydir_stream->d_name);
+    mydir_stream = readdir(dir);
+  }
+
+  closedir(dir);
+  return (0);
 }
