88ba1383fc32ec0dd0a568a06881ba77a5486f95
rkbrian <rkbriankong@gmail.com>
2021-07-14 21:44:06 +0000

    dir setup

---
+++ 0x01-ls/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+/**
+ * main - function to list the content of the current directory
+ * Retrun: 0
+ */
+int main(int argc, char *argv[])
+{
+  DIR *dir = opendir(".");
+  return 0;
+
+
+}
