d26efb3b65650b7b647bd7197ed41b8895c4d596
rkbrian <rkbriankong@gmail.com>
2021-07-20 16:27:26 +0000

    betty style

--- 0x01-ls/main.c
+++ 0x01-ls/main.c
@@ -9,46 +9,46 @@
 
 int main(int argc, char *argv[])
 {
-  int i, j, k = 0, l = 0;
-  char newlineflag = 'n', listallflag = 'n';
+	int i, j, k = 0, l = 0;
+	char newlineflag = 'n', listallflag = 'n';
 
-  for (i = 1; i < argc; i++)
-  {
-    if (argv[i][0] == '-')
-    {
-      for (j = 1; argv[i][j] != '\0'; j++)
-      {
-        if (argv[i][j] == '1' || argv[i][j] == 'l')
-          newlineflag = argv[i][j];
-        if (argv[i][j] == 'a' || argv[i][j] == 'A' )
-          listallflag = argv[i][j];
-      }
-    }
-    else
-    {
-      k++;
-      l++;
-    }
-  }
-  if (argc == 1 || (argc == 2 && argv[1][0] == '-'))
-    printme(NULL, newlineflag, listallflag);
-  else
-  {
-    for (i = 1; i < argc; i++)
-    {
-      if (argv[i][0] != '-')
-      {
-	if (l > 1)
-	  printf("%s:\n", argv[i]);
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i][0] == '-')
+		{
+			for (j = 1; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] == '1' || argv[i][j] == 'l')
+					newlineflag = argv[i][j];
+				if (argv[i][j] == 'a' || argv[i][j] == 'A')
+					listallflag = argv[i][j];
+			}
+		}
+		else
+		{
+			k++;
+			l++;
+		}
+	}
+	if (argc == 1 || (argc == 2 && argv[1][0] == '-'))
+		printme(NULL, newlineflag, listallflag);
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (argv[i][0] != '-')
+			{
+				if (l > 1)
+					printf("%s:\n", argv[i]);
 
-	printme(argv[i], newlineflag, listallflag);
-	if (k > 1)
-	  printf("\n");
-      }
-      k--;
-    }
-  }
-  return (0);
+				printme(argv[i], newlineflag, listallflag);
+				if (k > 1)
+					printf("\n");
+			}
+			k--;
+		}
+	}
+	return (0);
 }
 
 /**
@@ -60,41 +60,44 @@ int main(int argc, char *argv[])
 
 void printme(char *av, char newlineflag, char listallflag)
 {
-  int i = 0;
-  struct dirent *dstream;
-  DIR *dir;
-  char *separator, *newpath = NULL;
+	int i = 0;
+	struct dirent *dstream;
+	DIR *dir;
+	char *separator, *newpath = NULL;
 
-  if (av != NULL)
-  {
-    newpath = mall_strcat(".", av, "/");
-    dir = opendir(newpath);
-    free(newpath);
-  }
-  else
-    dir = opendir(".");
-
-  if (newlineflag == 'n')
-    separator = "  ";
-  else
-    separator = "\n";
-
-  dstream = readdir(dir);
-  while (dstream != NULL)
-  {
-    if ((listallflag == 'n' && (dstream->d_type == 4 || dstream->d_type == 8) && _strcmp(dstream->d_name, ".") != 0 && _strcmp(dstream->d_name, "..") != 0)
-	|| (listallflag == 'A' && _strcmp(dstream->d_name, ".") != 0 && _strcmp(dstream->d_name, "..") != 0) || (listallflag == 'a'))
-    {
-      if (i == 0)
-      {
-	printf("%s", dstream->d_name);
-	i++;
-      }
-      else
-	printf("%s%s", separator, dstream->d_name);
-    }
-    dstream = readdir(dir);
-  }
-  closedir(dir);
-  printf("\n");
+	if (av != NULL)
+	{
+		newpath = mall_strcat(".", av, "/");
+		dir = opendir(newpath);
+		free(newpath);
+	}
+	else
+		dir = opendir(".");
+	if (newlineflag == 'n')
+		separator = "  ";
+	else
+		separator = "\n";
+	dstream = readdir(dir);
+	while (dstream != NULL)
+	{
+		if ((listallflag == 'n' && (dstream->d_type == 4
+					    || dstream->d_type == 8)
+		     && _strcmp(dstream->d_name, ".") != 0
+		     && _strcmp(dstream->d_name, "..") != 0)
+		    || (listallflag == 'A' && _strcmp(dstream->d_name, ".") != 0
+			&& _strcmp(dstream->d_name, "..") != 0)
+		    || (listallflag == 'a'))
+		{
+			if (i == 0)
+			{
+				printf("%s", dstream->d_name);
+				i++;
+			}
+			else
+				printf("%s%s", separator, dstream->d_name);
+		}
+		dstream = readdir(dir);
+	}
+	closedir(dir);
+	printf("\n");
 }
--- 0x01-ls/readdir_to_str.c
+++ 0x01-ls/readdir_to_str.c
@@ -4,7 +4,7 @@
  * mall_strcat - function to cat strings for path, to be freed elsewhere
  * @oldstr: 1st major string
  * @newstr: 2nd major string
- * @separtor: string separator in form of minor string, can be null
+ * @separator: string separator in form of minor string, can be null
  * Return: new string
  */
 
@@ -14,7 +14,7 @@ char *mall_strcat(char *oldstr, char *newstr, char *separator)
 	char *dest;
 
 	if (oldstr == NULL)
-	  oldstr = "";
+		oldstr = "";
 
 	sublen = _strlen(oldstr) + _strlen(separator) + _strlen(newstr);
 	dest = malloc(sizeof(char) * sublen + 1);
@@ -28,13 +28,3 @@ char *mall_strcat(char *oldstr, char *newstr, char *separator)
 	_strcat(dest, newstr);
 	return (dest);
 }
-
-/**
- * pathfinder
- * 
- */
-
-/**
- * print_long - printing detail format of the file
- * void print_long(char *namestr, char *dirstr)
- */
