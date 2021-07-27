8daef44ed59ec36da684a3cc8280367f5d66166e
Ran Kong <rkbriankong@gmail.com>
2021-07-26 23:15:04 -0500

    getline stage 1

--- 0x02-getline/_getline.c
+++ 0x02-getline/_getline.c
@@ -3,10 +3,62 @@
 /**
  * _getline - reads an entire line from a file descriptor.
  * @fd: file descriptor
- * Return: fd line.
+ * Return: fd lines.
  */
-
 char *_getline(const int fd)
 {
-    ;
+    static char *buffer;
+    char *readtext;
+    static int linemem;
+    int laststop;
+
+    if (buffer == NULL) /* making buffer, this action is once for all */
+    {
+        buffer = malloc(sizeof(char) * READ_SIZE);
+        if (buffer == NULL)
+            return (NULL);
+        memset(buffer, 0, sizeof(char) * READ_SIZE); /* initializing buffer */
+        if (read(fd, buffer, READ_SIZE) == -1)
+            return (NULL);
+    }
+
+    laststop = linemem;
+    for (; buffer[linemem] != '\n' && buffer[linemem] != '\0'; linemem++)
+    {
+    }
+    if (buffer[linemem] == '\n') /* if stopped at new line, normal case */
+    {
+        readtext = continue_read(buffer, linemem, laststop);
+        laststop = linemem;
+        linemem++; /* memory count skip a space for null byte */
+    }
+    else if (buffer[linemem] == '\0') /* if stopped at the middle of the line */
+    {
+        if (linemem > laststop)
+            readtext = continue_read(buffer, linemem, laststop), laststop = linemem;
+        free(buffer);
+        buffer = NULL;
+    }
+    return (readtext);
+}
+
+/**
+ * continue_read - read through everything in a line and return
+ * @buffer: buffer pass down
+ * @linemem: the current end of text to be read
+ * @laststop: the current start of text been read, also the total size read
+ * Return: the text been read
+ */
+char *continue_read(char *buffer, int linemem, int laststop)
+{
+    char *readtext;
+    int i_diff = 0;
+
+    readtext = malloc(sizeof(char) * (linemem - laststop + 1));
+    if (readtext == NULL)
+        return (NULL);
+    for (; laststop < linemem; laststop++)
+        readtext[i_diff] = buffer[laststop];
+    readtext[i_diff] = '\0';
+    return (readtext);
 }
--- 0x02-getline/_getline.h
+++ 0x02-getline/_getline.h
@@ -1,15 +1,20 @@
 #ifndef _GETLINE_H
 #define _GETLINE_H
 
+#define READ_SIZE 1024
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
 
 /**
  * prototypes for static libraries
  */
 
 char *_getline(const int fd);
+char *continue_read(char *buffer, int linemem, int laststop);
 
 #endif
