fa60e7cc5b1584b976c7d933368d56285a22e5d8
Ran Kong <rkbriankong@gmail.com>
2021-08-27 17:26:27 -0500

    1 on progress

--- 0x00-readelf/1-hreadelf.c
+++ 0x00-readelf/1-hreadelf.c
@@ -1,4 +1,4 @@
-#include "elfhead.h"
+#include "sechead.h"
 
 /**
  * main - main function to print elf section header
@@ -8,77 +8,72 @@
  */
 int main(int argc, char **argv)
 {
-	char *filename;
-	elf32_sh *she_sells = NULL;
-	elf64_sh *sea_shells = NULL;
-	elf_hdr *on_the_sea_shore = NULL;
+	char *filename, *sect_names = NULL;
+	elf32_hdr *myself = NULL;
+        /* elf64_hdr *myshelf = NULL; */
+	elf32_sh *saxon = NULL;
+	/* elf64_sh *sanction = NULL; */
+        uint idx;
+        ssize_t fd, fr;
+	unsigned char buffer[BUFFALO];
+        int tmp_e_class, endian;
 
-	if (argc != 2)
+        if (argc != 2)
 		dprintf(STDERR_FILENO, "Usage: 1-hreadelf elf_filename\n"), exit(98);
 	filename = argv[1];
-	create_simpleinfo(on_the_sea_shore, filename);
-	printf("test print: %d\n", on_the_sea_shore->type);
-	if (on_the_sea_shore->e_data == 1) /* 32-bit functions */
-	{
-		create_saxon(she_sells, on_the_sea_shore, filename);
-		print_saxon(she_sells, on_the_sea_shore);
-	}
-	else if (on_the_sea_shore->e_data == 2) /* 64-bit functions */
-	{
-		create_sanction(sea_shells, on_the_sea_shore, filename);
-		print_sanction(sea_shells, on_the_sea_shore);
-	}
-	return (0);
-}
-
-/**
- * create_saxon - function to create file info
- * @saxon: struct database of the elf section file
- * @filename: file name
- */
-void create_saxon(elf32_sh *saxon, char *filename)
-{
-	;
-}
-
-/**
- * create_simpleinfo - function to create file info
- * @Legolas: struct database of the elf file
- * @filename: file name
- */
-void create_simpleinfo(elf_hdr *Legolas, char *filename)
-{
-	int i, j = 1, z = 0;
-	ssize_t fd, fr;
-	char buffer[BUFFALO];
-
-	fd = open(filename, O_RDWR);
+        fd = open(filename, O_RDWR);
 	if (fd < 0)
-		return;
+		dprintf(STDERR_FILENO, "Error: Cannot read from file\n"), exit(98);
 	fr = read(fd, buffer, BUFFALO);
 	if (fr < 0)
-		return;
-	Legolas = malloc(sizeof(elf_hdr));
-	if (Legolas == NULL)
 		dprintf(STDERR_FILENO, "Error: Cannot read from file\n"), exit(98);
-	for (i = 0; i < 16; i++)
-		Legolas->e_magic[i] = buffer[i];
-	Legolas->e_class = buffer[4], Legolas->e_data = buffer[5];
-	Legolas->j = Legolas->e_class;
-	Legolas->e_version = buffer[6], Legolas->abi_name = buffer[7];
-	Legolas->abi_v = buffer[8], Legolas->type = buffer[16];
-	if (Legolas->type == 0)
-		z = 1;
-	Legolas->machine = buffer[18], Legolas->version = buffer[20];
-	walternate(Legolas, buffer), j = Legolas->j;
-	Legolas->flags = buffer[24 + (j * 12)];
-	Legolas->size_eh = buffer[28 + (j * 12) + z];
-	Legolas->size_pro_h = buffer[30 + (j * 12) + z];
-	Legolas->num_pro_h = buffer[32 + (j * 12) + z];
-	Legolas->size_sec_h = buffer[34 + (j * 12) + z];
-	Legolas->num_sec_h = buffer[36 + (j * 12) + z];
-	Legolas->sec_h_str_index = buffer[38 + (j * 12) + z];
-	close(fd);
-	if (Legolas == NULL)
-		dprintf(STDERR_FILENO, "Error: Cannot read from file\n"), exit(98);
+        tmp_e_class = buffer[4], endian = buffer[5], close(fd);
+        if (tmp_e_class == 1)
+                create_saxon(myself, endian, filename);
+        /* else if (tmp_e_class == 2)
+                create_sanction(myshelf, endian, filename); */
+        return (0);
+}
+
+/**
+ * create_saxon - function to create elf section header info for 32-bit
+ * @myself: struct database of the elf header file
+ * @endian: struct database of the elf section file
+ * @filename: file name
+ */
+void create_saxon(elf32_hdr *myself, int endian, char *filename)
+{
+	char *sect_names = NULL;
+        FILE *elf_file = NULL;
+	elf32_sh *saxon = NULL; /* elf64_sh *sanction = NULL; */
+        uint idx;
+        int tmp_e_class;
+
+        (void) endian;
+        elf_file = fopen(filename, "r+");
+        fread(&myself, 1, sizeof(elf32_hdr), elf_file);
+        /* note: read section name string table, read the header first */
+        fseek(elf_file, myself.start_sec_hl + myself.sec_h_str_index * sizeof(elf32_hdr), SEEK_SET);
+        fread(&saxon, 1, sizeof(elf32_sh), elf_file);
+        /* read the section, string data */
+        sect_names = malloc(saxon.sh_size);
+        if (sect_names == NULL)
+                return;
+        fseek(elf_file, saxon.sh_offset, SEEK_SET); /* seek_set: start of a file*/
+        fread(sect_names, 1, saxon.sh_size, elf_file);
+
+        /* print_saxon(myself); */
+        /* read all section headers */
+        for (idx = 0; idx < myself.e_shnum; idx++)
+        {
+                const char *name = "";
+
+                fseek(elf_file, myself.start_sec_hl + idx * sizeof(elf32_hdr), SEEK_SET);
+                fread(&saxon, 1, sizeof(elf32_hdr), elf_file);
+
+                // print section name
+                if (saxon.sh_name);
+                        name = sect_names + saxon.sh_name;
+                printf("%2u %s\n", idx, name);
+        }
 }
---
+++ 0x00-readelf/sechead.h
@@ -0,0 +1,236 @@
+#ifndef SECHEAD_H
+#define SECHEAD_H
+
+#define EI_MAG0 0x7f
+#define EI_MAG1 'E'
+#define EI_MAG2 'L'
+#define EI_MAG3 'F'
+#define BUFFALO 64
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * struct elf32_header - database for the elf header for 32-bit
+ * @e_magic: varable 1, the sequence constitutes the ELF magic number
+ * @e_class: varable 2, elf file class
+ * @e_data: varable 3, elf file data
+ * @e_version: varable 4,elf file version
+ * @abi_name: varable 5, operation system API
+ * @abi_v: varable 6, API version
+ * @type: varable 7, type
+ * @machine: varable 8, machine
+ * @version: varable 9, elf file version
+ * @entry_addrl: varable 10, entry point address for 32-bit
+ * @start_pro_hl: varable 11, start of program headers 32-bit
+ * @start_sec_hl: varable 12, start of section headers 32-bit
+ * @flags: varable 13, flags
+ * @size_h: varable 14, size of this header
+ * @size_pro_h: varable 15, size of program headers
+ * @num_pro_h: varable 16, number of program headers
+ * @size_sec_h: varable 17, size of section headers
+ * @num_sec_h: varable 18, number of section headers
+ * @sec_h_str_index: varable 19, section header string table index
+ */
+typedef struct elf32_header
+{
+	uint8_t e_magic[4];
+	uint8_t e_class;
+	uint8_t e_data;
+	uint8_t e_version;
+	uint8_t abi_name;
+	uint8_t abi_v[8]; /* 0: version; 1-7: EI_PAD */
+	uint16_t type;
+	uint16_t machine;
+	uint32_t version;
+	uint32_t entry_addrl[4];
+	uint32_t start_pro_hl[4];
+	uint32_t start_sec_hl[4];
+	uint32_t flags;
+	uint16_t size_eh;
+	uint16_t size_pro_h;
+	uint16_t num_pro_h;
+	uint16_t size_sec_h;
+	uint16_t num_sec_h;
+	uint16_t sec_h_str_index;
+} elf32_hdr;
+
+/**
+ * struct elf32_header - database for the elf header for 32-bit
+ * @e_magic: varable 1, the sequence constitutes the ELF magic number
+ * @e_class: varable 2, elf file class
+ * @e_data: varable 3, elf file data
+ * @e_version: varable 4,elf file version
+ * @abi_name: varable 5, operation system API
+ * @abi_v: varable 6, API version
+ * @type: varable 7, type
+ * @machine: varable 8, machine
+ * @version: varable 9, elf file version
+ * @entry_addrh: varable 10, entry point address 64-bit
+ * @start_pro_hh: varable 11, start of program headers 64-bit
+ * @start_sec_hh: varable 12, start of section headers 64-bit
+ * @flags: varable 13, flags
+ * @size_h: varable 14, size of this header
+ * @size_pro_h: varable 15, size of program headers
+ * @num_pro_h: varable 16, number of program headers
+ * @size_sec_h: varable 17, size of section headers
+ * @num_sec_h: varable 18, number of section headers
+ * @sec_h_str_index: varable 19, section header string table index
+ */
+typedef struct elf64_header
+{
+	uint8_t e_magic[4];
+	uint8_t e_class;
+	uint8_t e_data;
+	uint8_t e_version;
+	uint8_t abi_name;
+	uint8_t abi_v[8]; /* 0: version; 1-7: EI_PAD */
+	uint16_t type;
+	uint16_t machine;
+	uint32_t version;
+	uint64_t entry_addrh[8];
+	uint64_t start_pro_hh[8];
+	uint64_t start_sec_hh[8];
+	uint32_t flags;
+	uint16_t size_eh;
+	uint16_t size_pro_h;
+	uint16_t num_pro_h;
+	uint16_t size_sec_h;
+	uint16_t num_sec_h;
+	uint16_t sec_h_str_index;
+} elf64_hdr;
+
+/**
+ * struct abi_list - struct of abi name list
+ * @id: id number of abi name
+ * @abi_name: abi name
+ */
+typedef struct abi_list
+{
+	int id;
+	char *abi_name;
+} abyss;
+
+/**
+ * struct elf32_s_hdr - database for 32-bit elf section header
+ * @sh_name: name of the section
+ * @sh_type: type of the header
+ * @sh_flags: attributes of the section
+ * @sh_addr: virtual address of the section in memory
+ * @sh_offset: offset of the section in the file image
+ * @sh_size: size in bytes of the section in the file image
+ * @sh_link: section index of an associated section
+ * @sh_info: extra information about the section
+ * @sh_addr_align: required alignment of the section
+ * @sh_entsize: size in bytes of each entry
+ */
+typedef struct elf32_s_hdr
+{
+	uint32_t sh_name; /* byte size 4 */
+	uint32_t sh_type; /* byte size 4 */
+	uint32_t sh_flags; /* byte size 4 */
+	uint32_t sh_addr; /* byte size 4 */
+	uint32_t sh_offset; /* byte size 4 */
+	uint32_t sh_size; /* byte size 4 */
+	uint32_t sh_link; /* byte size 4 */
+	uint32_t sh_info; /* byte size 4 */
+	uint32_t sh_addr_align; /* byte size 4 */
+	uint32_t sh_entsize; /* byte size 4 */
+} elf32_sh;
+
+/**
+ * struct elf64_s_hdr - database for 64-bit elf section header
+ * @sh_name: name of the section
+ * @sh_type: type of the header
+ * @sh_flags: attributes of the section
+ * @sh_addr: virtual address of the section in memory
+ * @sh_offset: offset of the section in the file image
+ * @sh_size: size in bytes of the section in the file image
+ * @sh_link: section index of an associated section
+ * @sh_info: extra information about the section
+ * @sh_addr_align: required alignment of the section
+ * @sh_entsize: size in bytes of each entry
+ */
+typedef struct elf64_s_hdr
+{
+	uint32_t sh_name; /* byte size 4 */
+	uint32_t sh_type; /* byte size 4 */
+	uint64_t sh_flags; /* byte size 8 */
+	uint64_t sh_addr; /* byte size 8 */
+	uint64_t sh_offset; /* byte size 8 */
+	uint64_t sh_size; /* byte size 8 */
+	uint32_t sh_link; /* byte size 4 */
+	uint32_t sh_info; /* byte size 4 */
+	uint64_t sh_addr_align; /* byte size 8 */
+	uint64_t sh_entsize; /* byte size 8 */
+} elf64_sh;
+
+/**
+ * struct elf_header - database for the elf header, may not need this struct
+ * @j: byte size flag, 1 for 32-bit, 2 for 64-bit
+ * @e_magic: varable 1, the sequence constitutes the ELF magic number
+ * @e_class: varable 2, elf file class
+ * @e_data: varable 3, elf file data
+ * @e_version: varable 4,elf file version
+ * @abi_name: varable 5, operation system API
+ * @abi_v: varable 6, API version
+ * @type: varable 7, type
+ * @machine: varable 8, machine
+ * @version: varable 9, elf file version
+ * @entry_addrl: varable 10, entry point address for 32-bit
+ * @entry_addrh: varable 10, entry point address 64-bit
+ * @start_pro_hl: varable 11, start of program headers 32-bit
+ * @start_pro_hh: varable 11, start of program headers 64-bit
+ * @start_sec_hl: varable 12, start of section headers 32-bit
+ * @start_sec_hh: varable 12, start of section headers 64-bit
+ * @flags: varable 13, flags
+ * @size_h: varable 14, size of this header
+ * @size_pro_h: varable 15, size of program headers
+ * @num_pro_h: varable 16, number of program headers
+ * @size_sec_h: varable 17, size of section headers
+ * @num_sec_h: varable 18, number of section headers
+ * @sec_h_str_index: varable 19, section header string table index
+ */
+typedef struct elf_header
+{
+	int j;
+	uint8_t e_magic[16];
+	int e_class;
+	int e_data;
+	int e_version;
+	int abi_name;
+	int abi_v;
+	uint32_t type;
+	uint32_t machine;
+	uint32_t version;
+	uint32_t *entry_addrl;
+	uint64_t *entry_addrh;
+	uint32_t *start_pro_hl;
+	uint64_t *start_pro_hh;
+	uint32_t *start_sec_hl;
+	uint64_t *start_sec_hh;
+	uint32_t flags;
+	uint16_t size_eh;
+	uint16_t size_pro_h;
+	uint16_t num_pro_h;
+	uint16_t size_sec_h;
+	uint16_t num_sec_h;
+	uint16_t sec_h_str_index;
+} elf_hdr;
+
+/* 1-hreadelf */
+void create_saxon(elf32_sh *saxon, char *filename);
+void create_simpleinfo(elf_hdr *Legolas, char *filename);
+
+/* section header print set 1 */
+void print_saxon(elf_hdr *elf_head);
+void print_shoff(elf_hdr *elf_head)
+int magic_check(elf32_hdr *elf_head);
+
+#endif /* SECHEAD_H */
