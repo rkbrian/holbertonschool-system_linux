db7cefa43a19544763bb27848958d8701c397d03
Ran Kong <rkbriankong@gmail.com>
2021-09-06 01:46:00 -0500

    1 on progress

--- 0x01-libasm/0-strlen.asm
+++ 0x01-libasm/0-strlen.asm
@@ -6,19 +6,19 @@ asm_strlen:
 	push	rbp ; push rbp register to stack
 	mov	rbp, rsp	; move rsp register content to rbp register 
 	mov	QWORD [rbp-24], rdi	; copy given str ptr dest to local rbp 24
-	mov	DWORD [rbp-4], 0	; initialize start number with 0
+	mov	DWORD [rbp-4], 0	; initialize index, null terminator if unused
 	jmp	.L2
 .L3:
 	inc	DWORD [rbp-4]	; increment of 1, in the loop condition
 .L2:
-	mov	edx, DWORD [rbp-4]	; int 0 saved to the top regular gpr
-	mov	rax, QWORD [rbp-24]	; what is this
-	add	rax, rdx	; what is this
-	movzx	eax, BYTE DWORD [rax]	; what is this
-	test	al, al	; compare with null byte
+	mov	edx, DWORD [rbp-4]	; index number saved to the top regular gpr
+	mov	rax, QWORD [rbp-24]	; str comes in
+	add	rax, rdx	; null byte comes after
+	movzx	eax, BYTE DWORD [rax]	; get the byte size of str ptr
+	test	al, al	; compare with null byte in rax
 	jne	.L3	; call L3 for not equal to null byte
-	jmp	.L4	; exit loop
+	jmp	.L4	; call L4 for case meet, exit loop
 .L4:
 	mov	eax, DWORD [rbp-4]	; loop finished, add 1 for the last byte
-	pop	rbp
+	pop	rbp	; end the function
 	ret
--- 0x01-libasm/1-strcmp.asm
+++ 0x01-libasm/1-strcmp.asm
@@ -6,22 +6,49 @@ asm_strcmp:
 	push	rbp
 	mov	rbp, rsp	; function start
 	mov	QWORD [rbp-24], rdi	; copy given str ptr1 dest to local rbp 24
-	mov	QWORD [rbp-32], rsi	; copy given str ptr2 dest to local rbp 24
-
-	
-	mov	DWORD [rbp-4], 0	; initialize start number with 0
-	jmp	.L2
+	mov	QWORD [rbp-32], rsi	; copy given str ptr2 dest to local rbp 32
+	mov	DWORD [rbp-4], 0	; init 1st index, null terminator if unused
+	jmp	.L2	; str1 length counting loop
 .L3:
-	inc	DWORD [rbp-4]	; increment of 1, in the loop condition
+	inc	DWORD [rbp-4]	; 1st index increment of 1 in the loop
 .L2:
-	mov	edx, DWORD [rbp-4]	; int 0 saved to the top regular gpr
-	mov	rax, QWORD [rbp-24]	; what is this
-	add	rax, rdx	; what is this
-	movzx	eax, BYTE DWORD [rax]	; what is this
+	mov	eax, DWORD [rbp-4]	; 1st index saved to the top regular gpr
+	movsx	rdx, eax	; signed-extand counter
+	mov	rax, QWORD [rbp-24]	; str1 comes in
+	add	rax, rdx	; null byte comes after
+	movzx	eax, BYTE DWORD [rax]	; get the byte size of str ptr
 	test	al, al	; compare with null byte
 	jne	.L3	; call L3 for not equal to null byte
-	jmp	.L4	; exit loop
+	mov	DWORD [rbp-8], 0	; for case meet, init 2nd index
+	jmp	.L4	; call L4 to next loop
+.L5:
+	inc	DWORD [rbp-8]	; 2nd index increment of 1
 .L4:
-	mov	eax, DWORD [rbp-4]	; loop finished, add 1 for the last byte
+	mov	eax, DWORD [rbp-8]	; 2nd index saved to the top regular gpr
+	movsx	rdx, eax	; signed-extand counter
+	mov	rax, QWORD [rbp-32]	; str2 comes in
+	add	rax, rdx	; null byte comes after
+	movzx	eax, BYTE DWORD [rax]	; get the byte size of str ptr
+	test	al, al	; compare with null byte
+	jne	.L5	; call L5 for not equal to null byte
+	mov	eax, DWORD [rbp-4]	; pull byte size saved in rbp-8
+	cmp	eax, DWORD [rbp-8]	; commpare with data saved in rbp-12
+	je	.L6	; if lengths are equal, do the char comparison
+	mov	eax, DWORD [rbp-4]	; do the comparison again for rbp-8 & -12
+	cmp	eax, DWORD [rbp-8]
+	jne	.L7	; if lengths aren't equal, return the diff in L7
+.L6:
+	mov	DWORD [rbp-12], 0	; init 3rd index
+	jmp	.L8	; call L4 to next loop
+
+.L10
+
+.L9:
+	inc	DWORD [rbp-12]	; 3rd index increment
+.L8:
+	mov	eax, DWORD [rbp-12]	; 3rd index saved to the top regular gpr
+	movsx	rdx, eax	; signed-extand counter
+	mov	rax, QWORD [rbp-24]	; str comes in
+.L7:
 	pop	rbp
 	ret
