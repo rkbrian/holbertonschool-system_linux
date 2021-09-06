f391ad6721f65eb53ccd8c098fa5f86466210e68
rkbrian <rkbriankong@gmail.com>
2021-09-06 18:59:25 +0000

    1: problem found, on progress

---
+++ 0x01-libasm/1-superman.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "libasm.h"
+
+#define S1  "Holberton School"
+#define S2  ""
+#define S3  "Holberton Socool"
+
+/**
+ * main - Program entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	printf("right: %d\n", strcmp(S1, S1));
+	printf("me: %d\n", asm_strcmp(S1, S1));
+	printf("right: %d\n", strcmp(S1, S2));
+	printf("me: %d\n", asm_strcmp(S1, S2));
+	printf("right: %d\n", strcmp(S1, S3));
+	printf("me: %d\n", asm_strcmp(S1, S3));
+
+	printf("All good!\n");
+	return (EXIT_SUCCESS);
+}
