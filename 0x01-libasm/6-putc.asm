745e6d34e484e897b9cbf49619e4a8237cd9fd87
rkbrian <rkbriankong@gmail.com>
2021-09-10 16:18:34 +0000

    call?

--- 0x01-libasm/6-putc.asm
+++ 0x01-libasm/6-putc.asm
@@ -1,23 +1,20 @@
 BITS 64
-	global asm_putc		; export for external files
-	extern main		; import main file and syscall
-	SECTION .texts		; this section stores code
+	global asm_putc
+	extern write
+	section .data
+
+
 asm_putc:
-	push	rbp			; push rbp register to stack
-	mov	rbp, rsp		; move rsp register content to rbp register
-	sub	rsp, 16			; alloc for return addr of local var for no frame ptr
-	mov	DWORD [rbp-4], edi	; copy int c to local rbp
-
-	mov	ebx, 1			; file descriptor of stdout, 1st input regi
-	lea	rcx, [rbp-4]		; int c saved to buffer
-	mov	edi, 1			; count of output, 3rd input regi
-	mov	eax, 1			; use the write syscall
-	syscall			; interupt with identifier of running write
-
-	mov	ebx, 0			; close file
-	mov	ecx, 0			; clear buffer
-	mov	eax, 60			; return fd of stdout
-	syscall			; interupt with identifier of running write
-
-	leave				; destroy stack frame
+	push    rbp
+	mov     rbp, rsp
+	sub     rsp, 16
+	mov     eax, edi
+	mov     BYTE [rbp-4], al
+	lea     rax, [rbp-4]
+	mov     edx, 1
+	mov     rsi, rax
+	mov     edi, 1
+	mov     eax, 0
+	call    write
+	leave
 	ret
