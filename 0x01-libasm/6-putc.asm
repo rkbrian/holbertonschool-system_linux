0d0f51e8ba3a2eabb8a972ec2dccbafcb6823354
rkbrian <rkbriankong@gmail.com>
2021-09-11 06:35:01 +0000

    6 and 7 debug

--- 0x01-libasm/6-putc.asm
+++ 0x01-libasm/6-putc.asm
@@ -1,20 +1,17 @@
 BITS 64
-	global asm_putc
-	extern write
-	section .data
-
-
+	global asm_putc	; export for external files
+	extern write	; import write file
+	section .text	; this section stores code
 asm_putc:
-	push    rbp
-	mov     rbp, rsp
-	sub     rsp, 16
-	mov     eax, edi
-	mov     BYTE [rbp-4], al
-	lea     rax, [rbp-4]
-	mov     edx, 1
-	mov     rsi, rax
-	mov     edi, 1
-	mov     eax, 0
-	call    write
-	leave
+	push	rbp
+	mov	rbp, rsp		; push rbp register to stack
+	sub	rsp, 16			; alloc for addr of local var for frame ptr
+	mov	DWORD [rbp-4], edi	; copy int c to local rbp and redeclare as char
+	lea	rax, [rbp-4]		; load effective address
+	mov	edx, 1			; write: 3rd var for write length
+	mov	rsi, rax		; write: 2nd var for print content
+	mov	edi, 1			; write: 1st var for file descriptor of stdout
+	mov	eax, 0			; clear buffer
+	call    write			; print char
+	leave				; destroy stack frame
 	ret
---
+++ 0x01-libasm/7-puts.asm
@@ -0,0 +1,23 @@
+BITS 64
+	global asm_puts			; export for external files
+	extern asm_strlen, write 	; import write file
+	SECTION .text			; this section stores code
+asm_puts:
+	push	rbp
+	mov	rbp, rsp		; push rbp register to stack
+	sub	rsp, 32			; alloc for addr of local var for frame ptr
+	mov	QWORD [rbp-24], rdi	; copy str ptr to local rbp
+	mov	rax, QWORD [rbp-24]	; load str
+	mov	rdi, rax		; str to be input of upcoming function
+	mov	eax, 0			; clear buffer
+	call	asm_strlen		; find str length, save the value to buffer
+	mov	DWORD [rbp-4], eax	; load buffer, int i = len(str)
+	mov	edx, DWORD [rbp-4]	; write: 3rd var for write length
+	mov	rax, QWORD [rbp-24]	; load str
+	mov	rsi, rax		; write: 2nd var for print content
+	mov	edi, 1			; write: 1st var for file descriptor of stdout
+	mov	eax, 0			; clear buffer
+	call	write			; print str
+	mov	eax, DWORD [rbp-4]	; return i
+	leave				; destroy stack frame
+	ret
