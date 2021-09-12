5e305d669faa2c7f821241da008a7d8761ae869e
rkbrian <rkbriankong@gmail.com>
2021-09-12 03:19:29 +0000

    8: debug set 2

--- 0x01-libasm/8-strcasecmp.asm
+++ 0x01-libasm/8-strcasecmp.asm
@@ -4,54 +4,122 @@ BITS 64
 asm_strcasecmp:		; setup & variables init
 	push	rbp
 	mov	rbp, rsp		; function start
-	mov	QWORD [rbp-24], rdi	; copy given str1 ptr dest to local rbp
-	mov	QWORD [rbp-32], rsi	; copy given str2 ptr dest to local rbp
-	jmp	.char_diff		; char comparison loop
-.int_lows:		; int buffers to save str1 and str2 char values
-	mov	rax, QWORD [rbp-24]	; str1 saved to the top regular gpr
+	mov	QWORD [rbp-8], rdi	; copy given str1 ptr dest to local rbp
+	mov	QWORD [rbp-16], rsi	; copy given str2 ptr dest to local rbp
+.s1null:		; if str1 is null
+	mov	rax, QWORD [rbp-8] 	; str1 saved to the top regular gpr
 	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	test	al, al			; find out if str1 is null
+	jne	.s2null			; if not null byte, check str2 null string
+	mov	rax, QWORD [rbp-16]	; str2 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
+	cmp	al, 64			; compare char diff for case
+	jle	.ret_02			; if not upper case, return 0 - str2
+	mov	rax, QWORD [rbp-16]	; str2 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
+	cmp	al, 90			; compare char diff for case
+	jg	.ret_02			; if not upper case, return 0 - str2
+	mov	rax, QWORD [rbp-16]	; str2 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
 	movsx	edx, al			; signed int for calc
-	mov	DWORD [rbp-4], eax	; int low1 = *str1
-	mov	rax, QWORD [rbp-32]	; str2 comes in
+	mov	eax, 32			; lower case int
+	sub	eax, edx		; return 32 - str2
+	jmp	.allisdone		; go to the end
+.ret_02:		; return 0 - str2
+	mov	rax, QWORD [rbp-16]	; str2 saved to the top regular gpr
 	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
 	movsx	eax, al			; signed int for calc
-	mov	DWORD [rbp-8], eax	; int low2 = *str2
-.lowers1:		; while loop to search char difference, str2 upper case
-	mov	rax, QWORD [rbp-24]	; str1 saved to the top regular gpr
+	neg	eax			; return 0 - str2
+	jmp	.allisdone		; go to the end
+.s2null:		; if str2 is null
+	mov	rax, QWORD [rbp-16] 	; str2 saved to the top regular gpr
 	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
-	cmp	eax, 65			; compare char diff for upper case
-	jle	.lowers2		; str1 <= 65, check str2
-	mov	rax, QWORD [rbp-24]	; str1 saved to the top regular gpr
+	test	al, al			; find out if str1 is null
+	jne	.char_diff		; if not null byte, go to char comparison loop
+	mov	rax, QWORD [rbp-8] 	; str1 saved to the top regular gpr
 	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
-	cmp	eax, 90			; compare char diff for upper case
-	jg	.lowers2		; str1 > 90, check str2
-	add	DWORD [rbp-4], 32	; low1 = low1 + 32
-.lowers2:		; while loop to search char difference, str2 lower case
-	mov	rax, QWORD [rbp-32]	; str2 saved to the top regular gpr
+	cmp	al, 64			; compare char diff for case
+	jle	.ret_10			; if not upper case, return str1
+	mov	rax, QWORD [rbp-8] 	; str1 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	cmp	al, 90			; compare char diff for case
+	jg	.ret_10			; if not upper case, return str1
+	mov	rax, QWORD [rbp-8] 	; str1 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	movsx	eax, al			; signed int for calc
+	add	eax, 32			; return 32 + str1
+	jmp	.allisdone		; go to the end
+.ret_10:		; return str1
+	mov	rax, QWORD [rbp-8] 	; str1 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	movsx	eax, al			; signed int for calc
+	jmp	.allisdone		; go to the end
+.s1big:			; while loop to search char difference, str1 lower case
+	mov	rax, QWORD [rbp-8]	; str1 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	movsx	edx, al			; signed int for calc
+	mov	rax, QWORD [rbp-16]	; str2 comes in
 	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
-	cmp	eax, 65			; compare char diff for upper case
-	jle	.check_diff		; str2 <= 65, ready to check difference
-	mov	rax, QWORD [rbp-24]	; str2 saved to the top regular gpr
+	movsx	ecx, al			; signed int for calc
+	mov	eax, edx		; value loaded for calc
+	sub	eax, ecx		; str1 char - str2 char
+	cmp	eax, 32			; compare char diff for case
+	jne	.s2big			; diff != 32, jump to str++
+	mov	rax, QWORD [rbp-8]	; str1 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	cmp	eax, 96			; compare char diff for case
+	jle	.s2big			; str1 > 96, jump to str++
+	mov	rax, QWORD [rbp-8]	; str1 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	cmp	eax, 122		; compare char diff for case
+	jg	.s2big			; str1 <= 122, jump to str++
+	jmp	.incs			; str++
+.s2big:			; while loop to search char difference, str2 lower case
+	mov	rax, QWORD [rbp-16]	; str2 saved to the top regular gpr
 	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
-	cmp	eax, 90			; compare char diff for upper case
-	jg	.check_diff		; str2 > 90, ready to check difference
-	add	DWORD [rbp-8], 32	; low2 = low2 + 32
-.check_diff:		; if statement to check char difference
-	mov	eax, DWORD [rbp-4] 	; load low1
-	cmp	eax, DWORD [rbp-8]	; compare with low2
-	je	.incs			; if equal, next chars
-	mov	eax, DWORD [rbp-4]	; load low1 to return position
-	sub	eax, DWORD [rbp-8]	; low1 - low2
-	jmp	.allisdone		; return result and go to the end
-.incs:			; next chars
-	inc	DWORD [rbp-24]		; str1 index increment for next char
-	inc	DWORD [rbp-32]		; str2 index increment for next char
-.char_diff:		; while loop to search char difference
+	movsx	edx, al			; signed int for calc
+	mov	rax, QWORD [rbp-8]	; str1 comes in
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	movsx	ecx, al			; signed int for calc
+	mov	eax, edx		; value loaded for calc
+	sub	eax, ecx		; str2 char - str1 char
+	cmp	eax, 32			; compare char diff for case
+	jne	.loop_start		; diff != 32, jump to str++
+	mov	rax, QWORD [rbp-16]	; str2 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
+	cmp	eax, 96			; compare char diff for case
+	jle	.loop_start		; str2 > 96, jump to str++
+	mov	rax, QWORD [rbp-16]	; str2 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
+	cmp	eax, 122		; compare char diff for case
+	jg	.loop_start		; str2 <= 122, jump to str++
+	jmp	.incs			; str++
+.loop_start:		; the if statement in the loop, jump out for diff of 2 strs
+	mov	rax, QWORD [rbp-8]	; str1 saved to the top regular gpr
+	movzx	edx, BYTE [rax]		; str1 byte qword ptr to get the char
+	mov	rax, QWORD [rbp-16]	; str2 comes in
+	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
+	cmp	dl, al			; comparison between str1 and str2 chars
+	jne	.ret_diff		; if greater, return ascii diff of chars
+.incs:					; next chars
+	inc	DWORD [rbp-8]		; str1 index increment for next char
+	inc	DWORD [rbp-16]		; str2 index increment for next char
+.char_diff:				; while loop to search char difference
 	mov	rax, QWORD [rbp-8]	; str1 saved to the top regular gpr
 	movzx	edx, BYTE [rax]		; str1 byte qword ptr to get the char
 	test	dl, dl			; str1 null byte check
-	je	.ret_zzz		; if null byte, return 0
-	jmp	.int_lows		; if all not equal to null byte, loop
+	je	.ret_diff		; if null byte, return ascii diff of chars
+	jmp	.s1big			; if all not equal to null byte, loop
+.ret_diff:		; return the string askii difference 1
+	mov	rax, QWORD [rbp-8] 	; str1 saved to the top regular gpr
+	movzx	eax, BYTE [rax]		; str1 byte qword ptr to get the char
+	movsx	edx, al			; signed int for calc
+	mov	rax, QWORD [rbp-16]	; str2 comes in
+	movzx	eax, BYTE [rax]		; str2 byte qword ptr to get the char
+	movsx	ecx, al			; signed int for calc
+	mov	eax, edx		; str1 char
+	sub	eax, ecx		; return str1 - str2
+	jmp	.allisdone		; go to the end
 .ret_zzz:		; return 0
 	mov	eax, 0			; return 0
 .allisdone:		; end the function
