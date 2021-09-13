b585cebe6c9947e111ef06f361596ba9267fdf45
Ran Kong <rkbriankong@gmail.com>
2021-09-12 22:59:55 -0500

    12 test

--- 0x01-libasm/12-strpbrk.asm
+++ 0x01-libasm/12-strpbrk.asm
@@ -6,78 +6,61 @@ asm_strpbrk:		; setup & variables init
 	mov	rbp, rsp		; function start
 	mov	QWORD [rbp-24], rdi	; copy given str s ptr dest to local rbp
 	mov	QWORD [rbp-32], rsi 	; copy given str accept ptr dest to local rbp
-.ifs1_null:		;check if s or accept is null string
+.ifs1_null:		; check if s or accept is null string
 	mov	rax, QWORD [rbp-24] 	; s saved to the top regular gpr
 	movzx	eax, BYTE [rax]		; s byte qword ptr to get the char
 	test	al, al			; checking null termination
-	je	.ret_zzz		; if equal to null byte, return 0
+	je	.ret_zzz		; if s[j] equal to null byte, return 0
 	mov	rax, QWORD [rbp-32] 	; accept saved to the top regular gpr
 	movzx	eax, BYTE [rax]		; accept byte qword ptr to get the char
+	test	al, al
+	je	.ret_zzz		; if accept[i] equal to null byte, return 0
+	mov	DWORD [rbp-8], 0	; init int j = 0
+	jmp	.outer_loop		; outer loop start
+.inner_start:		; nested loop start
+	mov	DWORD [rbp-4], 0	; init int i = 0
+	jmp	.inner_loop		; outer loop start
+.find_char:		; setup & variables init
+	mov	edx, DWORD [rbp-4]	; i saved to the top regular gpr
+	mov	rax, QWORD [rbp-32]	; accept saved to the top regular gpr
+	add	rax, rdx		; scan each element when index increases
+	movzx	edx, BYTE [rax]		; byte dword ptr to get the char from str
+	mov	ecx, DWORD [rbp-8]	; j saved to the top regular gpr
+	mov	rax, QWORD [rbp-24]	; s saved to the top regular gpr
+	add	rax, rcx
+	movzx	eax, BYTE [rax]
+	cmp	dl, al			; compare s[j] with accept[i]
+	je	.ret_null		; if s[j] = accept[i], check return value
+	inc	DWORD [rbp-4]		; i++
+.inner_loop:		; nested loop for next accept char
+	mov	edx, DWORD [rbp-4]	; i saved to the top regular gpr
+	mov	rax, QWORD [rbp-32]	; accept saved to the top regular gpr
+	add	rax, rdx
+	movzx	eax, BYTE [rax]
+	test	al, al			; checking accept[i] null termination
+	jne	.find_char		; if not null byte, run inner loop again
+	inc	DWORD [rbp-8]		; go to the outer loop, j++
+.outer_loop:		; outer loop for next s char
+	mov	edx, DWORD [rbp-8]	; j saved to the top regular gpr
+	mov	rax, QWORD [rbp-24]	; s saved to the top regular gpr
+	add	rax, rdx
+	movzx	eax, BYTE [rax]
+	test	al, al			; checking s[j] null termination
+	jne	.inner_start		; if not null byte, run outer loop again
+.ret_null:		; if statement for return value
+	mov	edx, DWORD [rbp-8] 	; j saved to the top regular gpr
+	mov	rax, QWORD [rbp-24]	; s saved to the top regular gpr
+	add	rax, rdx		; scan each element when index increases
+	movzx	eax, BYTE [rax]		; byte dword ptr to get the char from str
 	test	al, al			; checking null termination
-	je	.ret_zzz		; if equal to null byte, return 0
-	mov	DWORD [rbp-8], 0	; unsigned int 
-	jmp	.L2
-.L9:			;setup & variables init
-	mov	DWORD [rbp-4], 0
-	jmp	.L3
-.L6:			;setup & variables init
-	mov	edx, DWORD [rbp-4]
-	mov	rax, QWORD [rbp-32]
-	add	rax, rdx
-	movzx	edx, BYTE [rax]
-	mov	ecx, DWORD [rbp-8]
-	mov	rax, QWORD [rbp-24]
-	add	rax, rcx
-	movzx	eax, BYTE [rax]
-	cmp	dl, al
-	je	.L12
-	add	DWORD [rbp-4], 1
-.L3:			;setup & variables init
-	mov	edx, DWORD [rbp-4]
-	mov	rax, QWORD [rbp-32]
-	add	rax, rdx
-	movzx	eax, BYTE [rax]
-	test	al, al
-	jne	.L6
-	jmp	.L5
-.L12:			;setup & variables init
-	nop
-.L5:			;setup & variables init
-	mov	edx, DWORD [rbp-8]
-	mov	rax, QWORD [rbp-24]
-	add	rax, rdx
-	movzx	edx, BYTE [rax]
-	mov	ecx, DWORD [rbp-4]
-	mov	rax, QWORD [rbp-32]
-	add	rax, rcx
-	movzx	eax, BYTE [rax]
-	cmp	dl, al
-	je	.L13
-	add	DWORD [rbp-8], 1
-.L2:			;setup & variables init
-	mov	edx, DWORD [rbp-8]
-	mov	rax, QWORD [rbp-24]
-	add	rax, rdx
-	movzx	eax, BYTE [rax]
-	test	al, al
-	jne	.L9
-	jmp	.L8
-.L13:			;setup & variables init
-	nop
-.L8:			;setup & variables init
-	mov	edx, DWORD [rbp-8] 	;
-	mov	rax, QWORD [rbp-24]	;
-	add	rax, rdx		;
-	movzx	eax, BYTE [rax]		;
-	test	al, al			;
-	jne	.ret_charptr		;
+	jne	.ret_charptr		; if not null byte, return pointer to the char
 .ret_zzz:		; return zero for null string/byte
 	mov	eax, 0			; return 0
 	jmp	.all_finished		; go to end the function
-.ret_charptr:			; p
-	mov	edx, DWORD [rbp-8] 	;
-	mov	rax, QWORD [rbp-24]	;
-	add	rax, rdx		;
+.ret_charptr:			; return ptr to the first found char
+	mov	edx, DWORD [rbp-8] 	; j saved to the top regular gpr
+	mov	rax, QWORD [rbp-24]	; s saved to the top regular gpr
+	add	rax, rdx		; return &s[j]
 .all_finished:			; end the function
 	pop	rbp
 	ret
