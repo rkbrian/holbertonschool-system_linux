771ec26ad7e98cda07632961d7128bf17a68eb45
Ran Kong <rkbriankong@gmail.com>
2021-09-18 23:51:24 -0500

    7: test

---
+++ 0x00-signals/7-signal_send.c
@@ -0,0 +1,17 @@
+#include "signals.h"
+
+/**
+ * main - sends the signal SIGINT to a process, given its PID
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	int kill_ret;
+
+	if (argc != 2 || atoi(argv[1]) == 0)
+		printf("Usage: %s <pid>\n", argv[0]), exit(EXIT_FAILURE);
+	kill_ret = kill(atoi(argv[1]), SIGINT);
+	if (kill_ret == -1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
