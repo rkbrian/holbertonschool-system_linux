226312b92a1f23ad93a9118bdae71fab5b936e3a
rkbrian <rkbriankong@gmail.com>
2021-10-07 05:59:05 +0000

    1: going nowhere

--- 0x00-CPython/1-python.c
+++ 0x00-CPython/1-python.c
@@ -6,17 +6,45 @@
  */
 void print_python_list(PyObject *p)
 {
-int i, j = Py_Size(p);
+	/*PyTypeObject *plist = p->ob_type;*/
+	/* initialize PyList_Type instance */
+	static PyTypeObject PyList_Type = {
+		PyVarObject_HEAD_INIT(&PyList_Type, 0)
+		"list",
+		sizeof(PyListObject),
+		0, /* tp_itemsize */
+		0, /* tp_dealloc */
+		0, /* tp_print */
+		0, /* tp_getattr */
+		0, /* tp_setattr */
+		0, /* tp_as_async */
+		0, /* tp_repr */
+		0, /* tp_as_number */
+		0, /* tp_as_sequence */
+		0, /* tp_as_mapping */
+		0, /* tp_hash  */
+		0, /* tp_call */
+		0, /* tp_str */
+		0, /* tp_getattro */
+		0, /* tp_setattro */
+		0, /* tp_as_buffer */
+		Py_TPFLAGS_DEFAULT, /* tp_flags */
+		"List objects", /* tp_doc */
+	};
+	Py_ssize_t i, j = PyList_Size(PyList_Type->ob_item);
 
-if (&p != NULL)
-{
+	if (p != NULL) /* && Py_Is_TYPE(p, PyList_type) != 0)*/
+	{
 
-        printf("[*] Python list info\n");
-printf("[*] Size of the Python List = %zd\n", i);
-printf("[*] Allocated = %d\n", p->allocated);
-        for (i == 0; i < j; i++)
-        {
-                        printf("Element %zd: %s", i, typeof(p[i]));
-        }
-}
+		printf("[*] Python list info\n");
+		printf("[*] Size of the Python List = %zd\n", j);
+		printf("[*] Allocated = %zd\n", PyList_Type->allocated);
+		for (i = 0; i < j; i++)
+		{
+			printf("Element %zd: ", i);
+			/*if (p->ob_type)*/
+		/*printf("str\n");*/
+		/*printf("int\n");*/
+		}
+	}
 }
--- 0x00-CPython/cpython.h
+++ 0x00-CPython/cpython.h
@@ -1,8 +1,15 @@
 #ifndef CPYTHON_H
 #define CPYTHON_H
 
+#include <Python.h>
 #include <listobject.h>
 #include <object.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <assert.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
