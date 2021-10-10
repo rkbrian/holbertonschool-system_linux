4ba6bd0c5668b4e47e439ef45757b130401f5065
rkbrian <rkbriankong@gmail.com>
2021-10-10 20:26:40 +0000

    4: setup

---
+++ 0x00-CPython/4-python.c
@@ -0,0 +1,12 @@
+#include "cpython.h"
+
+/**
+ * print_python_string - print Python strings
+ * @p: pointer to the python object, treated as object in python
+ */
+void print_python_string(PyObject *p)
+{
+	PyListObject *pl;
+	Py_ssize_t i, j;
+
+}
