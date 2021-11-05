14de743af22d47cf19b6d383cede783ba64fa689
rkbrian <rkbriankong@gmail.com>
2021-11-05 00:17:39 +0000

    0 double value?

--- 0x00-strace/0-main.c
+++ 0x00-strace/0-main.c
@@ -11,26 +11,34 @@ int main(int argc, char *argv[], char *envp[])
 {
 	pid_t pid;
 	long ptval;
-	int i, j, status = 0;
+	int status = 0; /*, sysflag = 0;*/
 	struct user_regs_struct regs;
 
+	if (argc < 2)
+	{
+		fprintf(stderr, "Useage: %s command [args...]\n", argv[0]);
+		return (1);
+	}
+	setbuf(stdout, NULL);
 	pid = fork();
 	if (pid == 0)
 	{
-		ptrace(PTRACE_TRACEME, 0, 0, 0);
+		ptrace(PTRACE_TRACEME, pid, 0, 0);
 		execve(argv[1], argv + 1, envp);
 	}
 	else
 	{
-		wait(&status);
-		while (1)
+		status = 1;
+		while (!WIFEXITED(status)) /*  returns true if child terminated normally */
 		{
-			ptval = ptrace(PTRACE_SYSCALL, pid, 0, 0);
-			if (ptval)
-				printf("%ld\n", ptval);
-			sleep(1);
+			/* sysflag++;*/
+			ptrace(PTRACE_SYSCALL, pid, 0, 0);
+			wait(&status);
+			ptval = ptrace(PTRACE_GETREGS, pid, 0, &regs);
+			if (ptval != -1)
+				printf("%lu\n", (size_t)regs.orig_rax); /*printf("sysflag: %d\n", sysflag);*/
+			/*sleep(1);*/
 		}
-		/*ptrace(PTRACE_CONT, pid, NULL, NULL);*/
 	}
 	return (0);
 }
