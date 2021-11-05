ae3c6892a4b67dc5028dc447697963be98a5f936
Ran Kong <rkbriankong@gmail.com>
2021-11-05 08:10:12 -0500

    1 test

---
+++ 0x00-strace/1-main.c
@@ -0,0 +1,44 @@
+#include "syscalls.h"
+
+/**
+ * main - executes and traces a given command to print the syscall numbers
+ * @argc: argument count
+ * @argv: argument array
+ * @envp: environmental parameteres
+ * Return: 0 if success, 1 if failed
+ */
+int main(int argc, char *argv[], char *envp[])
+{
+	pid_t pid;
+	long ptval;
+	int status = 0; /*, sysflag = 0;*/
+	struct user_regs_struct regs;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Useage: %s command [args...]\n", argv[0]);
+		return (1);
+	}
+	setbuf(stdout, NULL);
+	pid = fork();
+	if (pid == 0)
+	{
+		ptrace(PTRACE_TRACEME, pid, 0, 0);
+		execve(argv[1], argv + 1, envp);
+	}
+	else
+	{
+		status = 1;
+		while (!WIFEXITED(status)) /*  returns true if child terminated normally */
+		{
+			/* sysflag++;*/
+			ptrace(PTRACE_SYSCALL, pid, 0, 0);
+			wait(&status);
+			ptval = ptrace(PTRACE_GETREGS, pid, 0, &regs);
+			if (ptval == 0)
+				printf("%s\n", syscalls_64_g[regs.orig_rax].name); /*printf("sysflag: %d\n", sysflag);*/
+			/*sleep(1);*/
+		}
+	}
+	return (0);
+}
