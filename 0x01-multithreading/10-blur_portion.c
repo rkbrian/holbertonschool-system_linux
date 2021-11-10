922cc4417c9f7fab6e9b79611d2e7ebdcc39f685
Ran Kong <rkbriankong@gmail.com>
2021-11-10 11:18:59 -0600

    progress saving

---
+++ 0x01-multithreading/10-blur_portion.c
@@ -0,0 +1,23 @@
+#include "multithreading.h"
+
+/**
+ * blur_portion - method blurs a portion of an image using a Gaussian Blur
+ * @portion: points to a data struct described in the project requirements
+ */
+void blur_portion(blur_portion_t const *portion)
+{
+	size_t i, j, rowend, colend; /* the ends that the  */
+
+	;
+}
+
+/**
+ * cutting_edge - function to define the edge of the matrix
+ * 
+ * Return:  
+ */
+int cutting_edge(blur_portion_t const *portion, )
+{
+	;
+
+}
