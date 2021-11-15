9cad304df6197c135c6ccb2a4a145ff3a18082cd
Ran Kong <rkbriankong@gmail.com>
2021-11-14 22:01:20 -0600

    6: progress

--- 0x01-multithreading/22-prime_factors.c
+++ 0x01-multithreading/22-prime_factors.c
@@ -1,1 +1,61 @@
 #include "multithreading.h"
+
+/**
+ * create_task - function to create a task
+ * @entry: pointer to the entry function of the task
+ * @param: the parameter that will later be passed to the entry function
+ * Return: a pointer to the created task structure
+ */
+task_t *create_task(task_entry_t entry, void *param)
+{
+	task_t *newtask = NULL;
+	static unsigned int id;
+
+	newtask = malloc(sizeof(task_t));
+	if (!newtask)
+		return (NULL);
+	newtask->entry = entry;
+	newtask->param = param;
+	newtask->status = PENDING;
+	newtask->result = NULL;
+	newtask->lock = task_mutex;
+	return (newtask);
+}
+
+/**
+ * destroy_task - function to destroy a task
+ * @task: 
+ */
+void destroy_task(task_t *task)
+{
+	if (task)
+	{
+		free(task);
+	}
+}
+
+/**
+ * exec_tasks - function serves as a thread entry,
+ * can safely return NULL as its return value will not be retrieved
+ * @tasks: pointer to the list of tasks to be executed
+ */
+void *exec_tasks(list_t const *tasks)
+{
+	;
+}
+
+/**
+ * init_mymutex - use pthread_mutex_init method as mutex constructor
+ */
+void init_tmutex(void)
+{
+	pthread_mutex_init(&task_mutex, NULL);
+}
+
+/**
+ * end_mymutex - use pthread_mutex_destroy method as mutex destructor
+ */
+void end_tmutex(void)
+{
+	pthread_mutex_destroy(&task_mutex);
+}
--- 0x01-multithreading/multithreading.h
+++ 0x01-multithreading/multithreading.h
@@ -11,7 +11,8 @@
 #include <sys/types.h>
 #include "list.h"
 
-pthread_mutex_t mymutex;
+pthread_mutex_t mymutex; /* for tprintf preventing race conditions */
+pthread_mutex_t task_mutex; /* for tasks of prime factor printing */
 
 /**
  * struct pixel_s - RGB pixel
@@ -75,13 +76,50 @@ typedef struct blur_portion_s
     kernel_t const *kernel;
 } blur_portion_t;
 
+/* Prime Factor V2 Structs */
+typedef void *(*task_entry_t)(void *);
+
+/**
+ * enum task_status_e - Task statuses
+ *
+ * @PENDING: Task is pending
+ * @STARTED: Task has been started
+ * @SUCCESS: Task has completed successfully
+ * @FAILURE: Task has completed with issues
+ */
+typedef enum task_status_e
+{
+    PENDING = 0,
+    STARTED,
+    SUCCESS,
+    FAILURE
+} task_status_t;
+
+/**
+ * struct task_s - Executable task structure
+ *
+ * @entry:  Pointer to a function to serve as the task entry
+ * @param:  Address to a custom content to be passed to the entry function
+ * @status: Task status, default to PENDING
+ * @result: Stores the return value of the entry function
+ * @lock:   Task mutex
+ */
+typedef struct task_s
+{
+    task_entry_t    entry;
+    void        *param;
+    task_status_t   status;
+    void        *result;
+    pthread_mutex_t lock;
+} task_t;
+
 /* functions in main */
 void *_thread_entry(__attribute__((unused))void *arg);
 void load_kernel(kernel_t *kernel, char const *file);
 void load_image(img_t *img, char const *file);
 void img_copy(img_t *dest, img_t const *src);
 void write_image(img_t const *img, char const *file);
-
+void *exec_tasks(list_t const *tasks);
 /* target functions */
 void *thread_entry(void *arg);
 int tprintf(char const *format, ...);
@@ -92,5 +130,10 @@ list_t *prime_factors(char const *s);
 void *_thread_entry(__attribute__((unused))void *arg);
 void init_mymutex(void) __attribute__((constructor)); /*my function*/
 void end_mymutex(void) __attribute__((destructor)); /*my function*/
+void print_task_result(task_t *task);
+task_t *create_task(task_entry_t entry, void *param);
+void destroy_task(task_t *task);
+void init_tmutex(void) __attribute__((constructor)); /*my function*/
+void end_tmutex(void) __attribute__((destructor)); /*my function*/
 
 #endif /* MULTITHREADING_H */
